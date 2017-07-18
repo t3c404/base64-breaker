a9211158c60057ccd1a073a4ac6ee6503a970439
Unknown <red.john@gmx.de>
2017-07-18 08:26:18 +0200

    added "foobar" string to textbox

--- js/not_found_handler.js
+++ js/not_found_handler.js
@@ -1,6 +1,6 @@
 setInterval(function(){
   $('#results td').each(function(){
-      if ($(this).text() == 'not found!') {
+      if ($(this).text() == 'not found') {
           $(this).css('background-color','#f33');
           $(this).css('font-weight','bold');
       }
